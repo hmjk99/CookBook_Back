9774a3d1ed5b75b243598ef5a5b212306155ad06
momot <motraore611@yahoo.com>
2023-04-14 00:07:53 -0400

    changes for edit functionality

--- app_api/views.py
+++ app_api/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
-from rest_framework import generics
+from django.shortcuts import render, get_object_or_404
+from rest_framework import generics, status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 from .serializers import UserProfileSerializer
 from .serializers import RecipeSerializer
 from .models import UserProfile
@@ -21,3 +23,13 @@ class RecipeList(generics.ListCreateAPIView):
 class RecipeDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Recipe.objects.all().order_by('id')
     serializer_class = RecipeSerializer
+
+def update_image(request, pk):
+    recipe = get_object_or_404(Recipe, pk=pk)
+    serializer = RecipeSerializer(recipe, data=request.data)
+    if serializer.is_valid():
+        if 'image' in request.data:
+            recipe.image.delete()
+        serializer.save()
+        return Response(serializer.data)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
