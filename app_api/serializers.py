fdfaa4a465601beb325586838e70ceae42ce1bbd
momot <motraore611@yahoo.com>
2023-04-17 12:31:16 -0400

    user auth attempt

--- app_api/serializers.py
+++ app_api/serializers.py
@@ -1,13 +1,20 @@
 from rest_framework import serializers
 from .models import UserProfile
 from .models import Recipe
+from .models import FavoriteRecipe
 
 class UserProfileSerializer(serializers.ModelSerializer):
     class Meta:
         model = UserProfile
-        fields= ('id', 'name', 'bio', 'image')
+        fields= ('id', 'username', 'bio', 'image')
 
 class RecipeSerializer(serializers.ModelSerializer):
     class Meta:
         model = Recipe
         fields = ('id', 'user_profile', 'title', 'image', 'instructions', 'equipment', 'ingredients')
+
+class FavoriteRecipeSerializers(serializers.ModelSerializer):
+
+    class Meta:
+        model = FavoriteRecipe
+        fields = ('id', 'user', 'recipe')
