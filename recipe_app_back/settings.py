abcdbe5c96dd1c5e90ec20650533d60a456646f3
Moonjoo Kim <124408498+hmjk99@users.noreply.github.com>
2023-04-19 13:26:25 -0400

    made changes to setting.py

--- recipe_app_back/settings.py
+++ recipe_app_back/settings.py
@@ -15,7 +15,7 @@ import os
 import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # Quick-start development settings - unsuitable for production
@@ -148,7 +148,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_URL = '/images/'
 
