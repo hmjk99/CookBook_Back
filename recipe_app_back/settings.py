2baa7851b6bd8e9271ed8d125c9fcf52e8721732
Moonjoo Kim <124408498+hmjk99@users.noreply.github.com>
2023-04-19 20:24:42 -0400

    deploy

--- recipe_app_back/settings.py
+++ recipe_app_back/settings.py
@@ -163,4 +163,4 @@ MEDIA_ROOT = os.path.join(BASE_DIR,  'static/images')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-CSRF_TRUSTED_ORIGINS = [ "http://localhost:3000"]
+CSRF_TRUSTED_ORIGINS = [ "http://localhost:3000", 'cookbook-group.herokuapp.com']
