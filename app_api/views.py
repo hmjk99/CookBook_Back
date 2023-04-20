cfb99da4639f1fe76b81fc2ec228eab71b7af32f
Moonjoo Kim <124408498+hmjk99@users.noreply.github.com>
2023-04-19 22:07:16 -0400

    deploy

--- app_api/views.py
+++ app_api/views.py
@@ -2,8 +2,7 @@ from django.shortcuts import render
 from django.contrib.auth import get_user_model, login, logout
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
-from rest_framework.authtoken.views import ObtainAuthToken
-from rest_framework.authtoken.models import Token
+from django.contrib.auth import authenticate
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import generics
